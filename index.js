c72dbd9206691b9586f6ea3fe93c83c7cd8d0c09
NoKinGG <34800430+sunyck@users.noreply.github.com>
2018-01-18 22:28:27 -0200

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,22 @@ const prefix = "#";
 bot.on("message", function(message) {
  if (msg.content === prefix + 'ping') {
    msg.reply('pong!');
-   msg..reply('caso pra mais comando digite #ajuda!');
+   msg.reply('caso pra mais comando digite #ajuda!');
  }
 })
 
-bot.login("NDAzNjIyMTc4NzMxNDU4NTYw.DUJ-DQ.gxk70DCFFgxBqCLjxyQHGzE5wLI");
+bot.login("const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix '#';
+
+client.on('ready', () => {
+  console.log('I am ready!');
+});
+
+client.on('message', message => {
+  if (message.content === 'ping') {
+    message.reply('pong');
+  }
+});
+
+client.login('NDAzNjk5Mjg3MjczMzczNzA2.DULNDg.8iR87gqqbZt_-cEJSbFs0lPUJkk');
