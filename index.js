955b859a988964c7c5e7a1646d2c421d9c0f19ac
NoKinGG <34800430+sunyck@users.noreply.github.com>
2018-01-18 22:26:43 -0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js");
+const bot = new Discord.Client();
+const prefix = "#";
+
+bot.on("message", function(message) {
+ if (msg.content === prefix + 'ping') {
+   msg.reply('pong!');
+   msg..reply('caso pra mais comando digite #ajuda!');
+ }
+})
+
+bot.login("NDAzNjIyMTc4NzMxNDU4NTYw.DUJ-DQ.gxk70DCFFgxBqCLjxyQHGzE5wLI");
